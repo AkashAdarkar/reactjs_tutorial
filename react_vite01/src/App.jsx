0149157c016d54d8b79f89c09683ae1c761dcd7d
AkashAdarkar <aregret01@gmail.com>
2024-08-12 20:54:08 +0530

    lesson1 completed

---
+++ react_vite01/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+    <input type="text" name="" id="name" />
+    <input type="button" value="Submit" id="submit" />
+
+    </>
+  )
+}
+
+export default App
